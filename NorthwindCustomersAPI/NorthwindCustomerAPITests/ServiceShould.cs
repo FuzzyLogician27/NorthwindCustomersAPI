6857c2e1335dc656ae08e98b8d11dc3787512e8c
aloopuri <thassan577@gmail.com>
2023-05-05 14:26:26 +0100

    added tests

--- NorthwindCustomersAPI/NorthwindCustomerAPITests/ServiceShould.cs
+++ NorthwindCustomersAPI/NorthwindCustomerAPITests/ServiceShould.cs
@@ -1,4 +1,5 @@
 ﻿
+using Castle.Core.Resource;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NorthwindCustomersAPI.Data.Repositories;
@@ -21,7 +22,7 @@ public class ServiceShould
     }
 
     [Category("Happy Path")]
-    [Category("GetSuppliers")]
+    [Category("GetCustomers")]
     [Test]
     public async Task GetAllAsync_WhenThereAreCustomers_ReturnsListOfCustomers()
     {
@@ -70,7 +71,7 @@ public class ServiceShould
     }*/
 
     [Category("Happy Path")]
-    [Category("GetSuppliers")]
+    [Category("GetCustomer")]
     [Test]
     public async Task GetAsync_WhenThereIsACustomer_ReturnsCustomer()
     {
@@ -87,7 +88,46 @@ public class ServiceShould
             .Returns(false);
 
         var _sut = new NorthwindService(mockLogger, mockRepository);
-        var result = await _sut.GetAllAsync();
-        Assert.That(result, Is.InstanceOf<IEnumerable<Customer>>());
+        var result = await _sut.GetAsync("Test");
+        Assert.That(result, Is.InstanceOf<Customer>());
     }
+
+    [Category("Happy Path")]
+    [Category("CreateCustomer")]
+    [Test]
+    public async Task CreateAsync_CreatingValidCustomer_ReturnsTrue()
+    {
+        var mockRepository = GetRepository();
+        var mockLogger = GetLogger();
+
+        var _sut = new NorthwindService(mockLogger, mockRepository);
+        var result = await _sut.CreateAsync(Mock.Of<Customer>());
+        Assert.That(result, Is.True);
+    }
+
+    //[Test]
+    //public async Task CreateAsync_CreatingValidCustomer_CallsMethodsCorrectNumberOfTimes()
+    //{
+    //    var mockRepository = GetRepository();
+    //    var mockLogger = GetLogger();
+        
+
+    //    List<Customer> customers = new List<Customer> { It.IsAny<Customer>() };
+
+    //    var cnt = customers.Count;
+    //    Mock.Get(mockRepository).Setup(cs => cs.Add(Mock.Of<Customer>()));
+
+
+    //    var _sut = new NorthwindService(mockLogger, mockRepository);
+    //     await _sut.CreateAsync(Mock.Of<Customer>());
+
+    //    Assert.That(customers.Count, Is.EqualTo(cnt + 1));
+
+
+    //    var mockCustomerService = new Mock<INorthwindService>();
+
+    //    mockCustomerService.Verify(mockRepository.Add(Mock.Of<Customer>()), Times.AtLeastOnce);
+
+    //}
+
 }
