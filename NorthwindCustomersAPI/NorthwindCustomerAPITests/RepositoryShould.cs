845f54439c6c19abc3d19cb09a11189ab5b02436
DanielleMassey <danimassey1998@gmail.com>
2023-05-05 12:43:52 +0100

    Added Tests

--- NorthwindCustomersAPI/NorthwindCustomerAPITests/RepositoryShould.cs
+++ NorthwindCustomersAPI/NorthwindCustomerAPITests/RepositoryShould.cs
@@ -66,4 +66,71 @@ public class RepositoryShould
         Assert.That(result, Is.TypeOf<Customer>());
         Assert.That(result.CompanyName, Is.EqualTo("Sparta Global"));
     }
+
+    [Category("Sad Path")]
+    [Category("FindAsync")]
+    [Test]
+    public void FindAsync_GivenInValidID_ReturnsNullCustomer()
+    {
+        var result = _sut.FindAsync("THSTJ").Result;
+        Assert.That(result, Is.Null);
+        
+    }
+
+    [Category("Happy Path")]
+    [Category("GetAllAsync")]
+    [Test]
+    public void GetAllAsync_ReturnsCustomersList()
+    {
+        var result = _sut.GetAllAsync().Result;
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.TypeOf<List<Customer>>());
+    }
+
+    [Category("Happy Path")]
+    [Category("Add")]
+    [Test]
+    public void Add_AddsCustomerToDatabase()
+    {
+        var countOfCustomers = _context.Customers.Count();
+        var c = new Customer()
+        {
+            CustomerId = "TESTC",
+            CompanyName = "Google",
+            City = "LA",
+            Country = "murica",
+            ContactName = "CEOMAN",
+            ContactTitle = "CEO",
+            Region = "W"
+        };
+        _sut.Add(c);
+        _context.SaveChanges();
+        Assert.That(_context.Customers, Is.Not.Null);
+        Assert.That(_context.Customers.ToList(), Has.Member(c));
+        Assert.That(_context.Customers.Count(), Is.EqualTo(countOfCustomers + 1));
+    }
+
+    //[Category("Sad Path")]
+    //[Category("Add")]
+    //[Test]
+    //public void Add_CustomerWithNoId_DoesNotAdd()
+    //{
+    //    var countOfCustomers = _context.Customers.Count();
+    //    var c = new Customer()
+    //    {
+    //        CompanyName = "Google",
+    //        City = "LA",
+    //        Country = "murica",
+    //        ContactName = "CEOMAN",
+    //        ContactTitle = "CEO",
+    //        Region = "W"
+    //    };
+    //    _sut.Add(c);
+    //    _context.SaveChanges();
+    //    //Assert.That(_context.Customers.ToList(), Has.No.Member(c));
+    //    Assert.That(_context.Customers.Count(), Is.EqualTo(countOfCustomers));
+    //}
+
+
+
 }
